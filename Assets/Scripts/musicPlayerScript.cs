c43492d781cc3e2ef8d4e9585e93992a7d631b63
RebeckaReblogga <elimail.kron@gmail.com>
2025-09-23 11:39:21 +0200

    added to the music player

--- Assets/Scripts/musicPlayerScript.cs
+++ Assets/Scripts/musicPlayerScript.cs
@@ -1,17 +1,39 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class musicPlayerScript : MonoBehaviour
 {
-    [SerializeField] AudioSource mainLayer;
-    [SerializeField] AudioSource stressLayer;
+    [SerializeField] AudioClip mainLayerClip;
+    [SerializeField] AudioClip stressLayerClip;
+    private AudioSource mainLayer;
+    private AudioSource stressLayer;
 
+    private float speed = 1;
+    private float speedStressRelation = 0.2f;
+    public float stressLevel = 0;
+    public float musicVolume; //volume of the music
     void Start()
     {
+        //creating the audio source components and playing the music
+        mainLayer =  gameObject.AddComponent<AudioSource>();
+        stressLayer = gameObject.AddComponent<AudioSource>();
+        mainLayer.clip = mainLayerClip;
+        stressLayer.clip = stressLayerClip;
+
+        mainLayer.volume = musicVolume;
+        stressLayer.volume = stressLevel * musicVolume;
+
         mainLayer.Play();
         stressLayer.Play();
     }
     void Update()
     {
+
+        speed = 1 + (stressLevel * speedStressRelation);
+        mainLayer.pitch = speed;
+        stressLayer.pitch = speed;
+
+        stressLayer.volume = stressLevel * musicVolume;
         if (!mainLayer.isPlaying)
         {
             mainLayer.Stop();
