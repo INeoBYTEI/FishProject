08b7601528451bccd15792ca49a5c38ea3d1a7e3
PossumOverlord <teomanbayindir@gmail.com>
2025-09-23 13:16:16 +0200

    Switched to 2d colliders

--- Assets/FishMovement.cs
+++ Assets/FishMovement.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
-    Rigidbody rb;
+    Rigidbody2D rb;
+    public int speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector3(1, 1, 0);
+
     }
     void OnCollisionEnter(Collision collision)
     {
-        rb.linearVelocity = rb.linearVelocity * -1;
+        
     }
 }
