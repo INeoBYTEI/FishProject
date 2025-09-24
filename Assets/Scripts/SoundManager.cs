bf072a9eab2b1aa5e380ebb706f6e4f9cd6edca9
RebeckaReblogga <elimail.kron@gmail.com>
2025-09-24 09:43:38 +0200

    created mixergroup system, so now you could more easily implement audio settings

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -13,14 +14,27 @@ using UnityEngine.Audio;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
+    [SerializeField] AudioMixer mainMix;
+
+    public AudioMixerGroup SFXMixGroup;
+    public AudioMixerGroup MusicMixGroup;
     void Start()
     {
         if (!instance)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            SFXMixGroup = mainMix.FindMatchingGroups("Music")[0];
+            MusicMixGroup = mainMix.FindMatchingGroups("SFX")[0];
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
-    public void CreateSound(AudioClip audioClip, float volume = 0.6f, AudioMixerGroup audioMixerGroup = null)
+    
+    public void CreateSound(AudioClip audioClip, float volume = 0.6f, AudioMixerGroup audioMixerGroup = null, Vector3 position = new Vector3())
     {
         GameObject soundToCreate = new GameObject("soundEffect");
         soundToCreate.SetActive(false);
@@ -32,6 +46,7 @@ public class SoundManager : MonoBehaviour
             audioSource.outputAudioMixerGroup = audioMixerGroup;
         }
         soundToCreate.SetActive(true);
+        soundToCreate.transform.position = position;
         Destroy(soundToCreate, audioSource.clip.length);
     }
 }
